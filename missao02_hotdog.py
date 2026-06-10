ed5e6873a102993067ff9de7e930f9c88b476280
gustavopga04-commits <gustavopga04@gmail.com>
2026-06-10 18:43:50 -0300

    missao01 - ficha do Cruzeiro

---
+++ missao02_hotdog.py
@@ -0,0 +1,8 @@
+nome = input("Qual é o seu nome? ")
+print(f"Olá, {nome}! ")
+
+quantidade = int(input("Foram quantos vendidos? "))
+preco = float(input("Qual o valor? "))
+
+total = quantidade * preco
+print(f"Total do dia: R$ {total}")
