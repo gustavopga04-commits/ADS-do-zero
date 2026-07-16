117e19e924365d32a690f931d60fc257b04f4a6e
gustavopga04-commits <gustavopga04@gmail.com>
2026-07-16 19:38:45 -0300

    Adiciona exercicio de pre e pos incremento/decremento (++ e --)

---
+++ c/pos-incremento.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    /*
+    incremento (++)
+    pre-decremento ++a
+    pos-decremento a++
+    decremento (--)
+    pre-decremento --a
+    pos-decremento a--
+    */
+
+    int numero1 = 1, resultado;
+
+    printf("Antes de incremento: %d\n", numero1);
+    // numero1 = numero1 +1;
+    //numero1 += 1;
+    //pos incremento:
+    //resultado = numero1;
+    //numero++;
+    resultado = numero1++;
+    //printf("Apos pos-incremento: %d\n", numero1);
+    printf("Apos pos-incremento - numero 1: %d - Resultado: %d\n", numero1, resultado);
+    
+    resultado = ++numero1;
+    //printf("Apos pos-incremento: %d\n", numero1);
+    printf("Apos pre-incremento - numero 1: %d - Resultado: %d\n", numero1, resultado);
+    
+    // numero1 = numero1 -1;
+    //numero1 -= 1;
+//   numero1--;
+//   printf("Apos pos-decremento - numero 1: %d - Resultado: %d\n", numero1, resultado);
+    resultado = numero1--;
+    printf("Apos pos-decremento - numero 1: %d - Resultado: %d\n", numero1, resultado);
+
+    resultado = --numero1;
+    printf("Apos pre-decremento - numero 1: %d - Resultado: %d\n", numero1, resultado);
+    return 0;
+}
